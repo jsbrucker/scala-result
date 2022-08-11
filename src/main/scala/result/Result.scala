e6a8a921237f539b9e5a398a5160e982510660d3
John Brucker <jsbrucker@gmail.com>
2022-08-11 01:38:43 -0400

    feat(is): add is* methods

--- src/main/scala/result/Result.scala
+++ src/main/scala/result/Result.scala
@@ -5,10 +5,89 @@ package result
   * [[Result]] is a type that represents either success ([[Ok]]) or failure ([[Err]]).
   * See the [[result package documentation]] for details.
   */
-sealed trait Result[+T, +E] extends Any
+sealed trait Result[+T, +E] extends Any {
+
+  /** Returns `true` if the result is [[Ok]].
+    *
+    * ==Examples==
+    *
+    * {{{
+    * val x: Result[Int, String] = Ok(-3)
+    * assert(x.isOk == true)
+    *
+    * val y: Result[Int, String] = Err("Some error message")
+    * assert(y.isOk == false)
+    * }}}
+    */
+  def isOk: Boolean
+
+  /** Returns `true` if the result is [[Ok]] and the value inside of it matches a predicate.
+    *
+    * ==Examples==
+    *
+    * {{{
+    * val x: Result[Int, String] = Ok(2)
+    * assert(x.isOkAnd(_ > 1) == true)
+    *
+    * val y: Result[Int, String] = Ok(0)
+    * assert(y.isOkAnd(_ > 1) == false)
+    *
+    * val z: Result[Int, String] = Err("hey")
+    * assert(z.isOkAnd(_ > 1) == false)
+    * }}}
+    */
+  def isOkAnd(f: T => Boolean): Boolean
+
+  /** Returns `true` if the result is [[Err]].
+    *
+    * ==Examples==
+    *
+    * {{{
+    * val x: Result[Int, String] = Ok(-3)
+    * assert(x.isErr == false)
+    *
+    * val y: Result[Int, String] = Err("Some error message")
+    * assert(y.isErr == true)
+    * }}}
+    */
+  def isErr: Boolean
+
+  /** Returns `true` if the result is [[Err]] and the value inside of it matches a predicate.
+    *
+    * ==Examples==
+    *
+    * {{{
+    * val x: Result[String, Int] = Err(2)
+    * assert(x.isErrAnd(_ > 1) == true)
+    *
+    * val y: Result[String, Int] = Err(0)
+    * assert(y.isErrAnd(_ > 1) == false)
+    *
+    * val z: Result[String, Int] = Ok("Some success string")
+    * assert(z.isErrAnd(_ > 1) == false)
+    * }}}
+    */
+  def isErrAnd(f: E => Boolean): Boolean
+}
 
 /** Contains the success value */
-case class Ok[+T, +E](v: T) extends AnyVal with Result[T, E]
+case class Ok[+T, +E](v: T) extends AnyVal with Result[T, E] {
+  override def isOk: Boolean = true
+
+  override def isOkAnd(f: T => Boolean): Boolean = f(v)
+
+  override def isErr: Boolean = false
+
+  override def isErrAnd(f: E => Boolean): Boolean = false
+}
 
 /** Contains the error value */
-case class Err[+T, +E](e: E) extends AnyVal with Result[T, E]
+case class Err[+T, +E](e: E) extends AnyVal with Result[T, E] {
+  override def isOk: Boolean = false
+
+  override def isOkAnd(f: T => Boolean): Boolean = false
+
+  override def isErr: Boolean = true
+
+  override def isErrAnd(f: E => Boolean): Boolean = f(e)
+}
--- src/main/scala/result/package.scala
+++ src/main/scala/result/package.scala
@@ -7,6 +7,10 @@
   *
   * Functions should return [[Result]] whenever errors are expected and recoverable.
   *
+  * For simplicity many examples make use of primitives such as `String` and `Int` for the error type. It is recommended
+  * that in practice developers should try to make use of more structured types to allow for improved error handling. As
+  * opposed to relying on a stringly-typed interface or integer error codes.
+  *
   * A simple function returning [[Result]] might be defined and used like so:
   *
   * {{{
@@ -36,6 +40,30 @@
   *   case Err(e) => println(s"error parsing header: \$e")
   * }
   * }}}
+  *
+  * Pattern matching on [[Result]]s is clear and straightforward for simple cases, but [[Result]] comes with some
+  * convenience methods that make working with it more succinct.
+  *
+  * {{{
+  * val goodResult: Result[Int, String] = Ok(10);
+  * val badResult: Result[Int, String] = Err("Some Error");
+  *
+  * // The `isOk` and `isErr` methods do what they say.
+  * assert(goodResult.isOk && !goodResult.isErr)
+  * assert(badResult.isErr && !badResult.isOk)
+  * }}}
+  *
+  * =Method overview=
+  *
+  * In addition to working with pattern matching, [[Result]] provides a wide variety of different methods.
+  *
+  * ==Querying the variant==
+  *
+  * The [[Result.isOk isOk]] and [[Result.isErr isErr]] methods return `true` if the [[Result]] is [[Ok]] or [[Err]],
+  * respectively.
+  *
+  * The [[Result.isOkAnd isOkAnd]] and [[Result.isErrAnd isErrAnd]] take in a predicate and return `true` if the
+  * [[Result]] is [[Ok]] or [[Err]] respectively, and the predicate returns `true` when applied to the contained value.
   *
   * @note
   * This documentation is a derivative of the [[https://doc.rust-lang.org/std/result/ Rust Result<T, E> documentation]]
