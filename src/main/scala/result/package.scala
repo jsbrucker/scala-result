b4fadfe62f0207cb419db878385e829a52036c4a
John Brucker <jsbrucker@gmail.com>
2022-10-13 23:41:30 -0400

    feat(iterable): add partitionResult

--- src/main/scala/result/package.scala
+++ src/main/scala/result/package.scala
@@ -1,3 +1,5 @@
+import scala.math.Ordering
+
 /** =Use Case=
   *
   * Error handling with the `Result` type.
@@ -370,6 +372,35 @@ package object result {
     def toResult: Result[Throwable, T] = self match {
       case scala.util.Success(ok) => Ok(ok)
       case scala.util.Failure(e)  => Err(e)
+    }
+  }
+
+  /** Implicit conversion wrapper for iterable collections of [[Result]]`[E, T]` */
+  implicit class ResultIterableOps[E, T, +CC[_], +C](
+      val self: scala.collection.IterableOps[Result[E, T], CC, C]
+  ) {
+
+    /** A pair of, first, all [[Ok]] elements and, second, all [[Err]] elements.
+      *
+      * Has the same limitations as `partition` on the given collection.
+      *
+      * ==Examples==
+      *
+      * {{{
+      * >>> val (oks, errs) = Vector(Ok(1), Err("A"), Ok(2), Ok(3), Err("B")).partitionResult
+      *
+      * >>> oks == Vector(1, 2, 3)
+      * true
+      *
+      * >>> errs == Vector("A", "B")
+      * true
+      * }}}
+      */
+    def partitionResult[DD[_], D](implicit
+        ev: C <:< scala.collection.IterableOps[Result[E, T], DD, D]
+    ): (DD[T], DD[E]) = {
+      val (oks, errs) = self.partition(_.isOk)
+      (ev(oks).map(_.unwrap), ev(errs).map(_.unwrapErr))
     }
   }
 }
